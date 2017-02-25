a21071f0f05154785cb8762d6a681b992f8a2716
Todd <tchar@umich.edu>
2017-02-25 17:55:00 -0500

    Improved client server communication with threading

--- client.py
+++ client.py
@@ -1,35 +1,43 @@
 import socket
 import sys
+import time
 
 #Arguments to Client.
 #1. Port id to connect to first replica of server
 #2. ClientID number. Should be unique
 
-s = socket.socket()
-host = socket.gethostname()
+
+
 port = int(sys.argv[1].strip())
 clientID = int(sys.argv[2].strip())
 seqNum = 0
 
-s.settimeout(2)
-
-if s.connect_ex((host,port)) != 0:
-	print 'Could not connect to port', port
 
 while(1):
+  s = socket.socket()
+  s.settimeout(2)
+
+  if s.connect_ex((socket.gethostname(),port)) != 0:
+	print 'Could not connect to port', port
+  print "connected to server"
+
+  #chat = "hello world"
   chat = raw_input("Enter text to chat (or q to quit): ")
   if chat == "q":
     s.close
     exit()
 
   msg = str(clientID) + "|" +  str(seqNum) + "|" + str(chat)
-  header = str(clientID) + "|" + str(seqNum) + "|" + str(sys.getsizeof(msg)) + "$"
+  header = str(clientID) + "|" + str(seqNum) + "|" + str(len(msg)) + "$"
 
   print header
   print msg
 
   s.sendall(header)
+  print 'Sent header'
+ # time.sleep(0.5)
   s.sendall(msg)
+  print 'Sent message'
 
   buf = ""
   resp = ""
@@ -38,3 +46,5 @@ while(1):
       buf = s.recv(1, socket.MSG_WAITALL)
 
   print resp
+  seqNum += 1
+  s.close()
--- server.py
+++ server.py
@@ -1,29 +1,44 @@
 import socket
 import sys
 import time
+import thread
 
-seq_num = 0
-s = socket.socket()
-host = socket.gethostname()
-port = int(sys.argv[1].strip())
-s.bind((host, port))
-
-s.listen(5)
-while True:
-    c, addr = s.accept()
+def processRequest(conn, addr, seq_num):
     print 'Got connection from', addr
     buf = ""
     header = ""
     while buf != "$":
     	header += buf
-    	buf = s.recv(1, socket.MSG_WAITALL)
+    	buf = conn.recv(1, socket.MSG_WAITALL)
     header = header.split('|')
     clientID = int(header[0])
     clientSeq = int(header[1])
-    messageSize = int(header[3])
-    message = s.recv(messageSize, socket.MSG_WAITALL)
-    seq_num += 1
+    messageSize = int(header[2])
+    message = conn.recv(messageSize, socket.MSG_WAITALL)
     log = str(seq_num) + message
     msg = str(clientSeq) + '$'
-    c.send(msg)
-    c.close()
+    conn.send(msg)
+    conn.close()
+
+def start():
+  s = socket.socket()
+  host = socket.gethostname()
+  port = int(sys.argv[1].strip())
+  s.bind((host, port))
+  seq_num = 0
+
+  while True:
+      s.listen(5)
+      c, addr = s.accept()
+
+      #processRequest(c,addr, seq_num)
+      try:
+          thread.start_new_thread( processRequest, (c, addr, seq_num))
+          seq_num += 1
+      except: 
+          print 'Cannot start thread'
+
+
+if __name__ == "__main__":
+  start()
+
