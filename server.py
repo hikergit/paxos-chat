1d6099abb7e3ac2ad0104a15c411aabd32badddd
Todd <tchar@umich.edu>
2017-02-25 18:43:08 -0500

    Added queue to server to handle multiple client requests

--- server.py
+++ server.py
@@ -2,24 +2,36 @@ import socket
 import sys
 import time
 import thread
+import Queue
+  
+requests = Queue.Queue()
 
-def processRequest(conn, addr, seq_num):
-    print 'Got connection from', addr
-    buf = ""
-    header = ""
-    while buf != "$":
-    	header += buf
-    	buf = conn.recv(1, socket.MSG_WAITALL)
-    header = header.split('|')
-    clientID = int(header[0])
-    clientSeq = int(header[1])
-    messageSize = int(header[2])
-    message = conn.recv(messageSize, socket.MSG_WAITALL)
-    log = str(seq_num) + '|' + message
-    print log
-    msg = str(clientSeq) + '$'
-    conn.send(msg)
-    conn.close()
+def service():
+
+  seq_num = 0
+  global requests
+
+  while(1):
+    conn = requests.get()
+    processRequest(conn, seq_num)
+
+def processRequest(conn, seq_num):
+  buf = ""
+  header = ""
+  while buf != "$":
+      header += buf
+      buf = conn.recv(1, socket.MSG_WAITALL)
+  header = header.split('|')
+  clientID = int(header[0])
+  clientSeq = int(header[1])
+  messageSize = int(header[2])
+  message = conn.recv(messageSize, socket.MSG_WAITALL)
+  log = str(seq_num) + '|' + message
+  print log
+  msg = str(clientSeq) + '$'
+  seq_num += 1
+  conn.send(msg)
+  conn.close()
 
 def start():
   s = socket.socket()
@@ -27,19 +39,22 @@ def start():
   port = int(sys.argv[1].strip())
   s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   s.bind((host, port))
-  seq_num = 0
 
+
+  try:
+      thread.start_new_thread( service,  () )
+  except: 
+    print 'Cannot start thread'
+
+
+  global requests
   while True:
       s.listen(5)
       c, addr = s.accept()
 
-      #processRequest(c,addr, seq_num)
-      try:
-          thread.start_new_thread( processRequest, (c, addr, seq_num))
-          seq_num += 1
-      except: 
-          print 'Cannot start thread'
-
+      requests.put(c)
+       
+      
 
 if __name__ == "__main__":
   start()
