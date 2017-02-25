980c9ce338e9fbd560831646e59a3a5f8021e204
hikergit <hikergit@users.noreply.github.com>
2017-02-25 16:27:33 -0500

    Single server done

--- server.py
+++ server.py
@@ -12,7 +12,18 @@ s.listen(5)
 while True:
     c, addr = s.accept()
     print 'Got connection from', addr
+    buf = ""
+    header = ""
+    while buf != "$":
+    	header += buf
+    	buf = recv(1, 'MSG_WAITALL')
+    header = header.split('|')
+    clientID = int(header[0])
+    clientSeq = int(header[1])
+    messageSize = int(header[3])
+    message = recv(messageSize, 'MSG_WAITALL')
     seq_num += 1
-    msg = 'Thank you for connecting. Your seq number is ' + str(seq_num)
+    log = str(seq_num) + message
+    msg = str(clientSeq) + '$'
     c.send(msg)
     c.close()
