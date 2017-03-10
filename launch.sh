efb54874f68165e1f3d22fae0e074bc72ca8c47a
Todd <tchar@umich.edu>
2017-03-09 23:34:49 -0500

    Fixed script. Can add skipped sequence number

--- launch.sh
+++ launch.sh
@@ -3,15 +3,20 @@
 #Arguments
 #1. N - number of servers
 #2. Starting port number for replicas. Increments by one for each replica
+#3. Optional argument. This seq number will be skipped in execution
 
-f="$1"
+n="$1"
 id=0
 port="$2"
+
+if [ -z "$3" ]; then skip=-1; else skip="$3"; fi
 server="server.py"
 OUT='config.txt'
 if [ $( ls $OUT ) ]; then rm $OUT; fi
 
-while [ $id -lt $f ]; 
+# Write config file
+echo $skip >> $OUT 
+while [ $id -lt $n ]; 
 do
   echo $HOSTNAME $port >> $OUT
   
@@ -20,6 +25,16 @@ do
 
 done
 
+# Bring up the replicas
+id=0
+port=$2
+while [ $id -lt $n ]; 
+do
+  python $server $port $id &
+  let port=port+1
+  let id=id+1
+
+done
 
 
-python $server $port $id &
+
