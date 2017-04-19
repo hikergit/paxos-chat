4cb42f44484e865919b7f71109fd638b0ef9abea
hiker <hiker@umich.edu>
2017-04-18 21:22:29 -0400

    maclaunchMaster working

--- maclaunchMaster.sh
+++ maclaunchMaster.sh
@@ -10,34 +10,40 @@ echo "#Arguments
 exit 1
 fi
 
+
+#!/bin/bash
+
+#Arguments
+#1. N - number of shards
+#2. M - number of replicas
+#3. Starting port number for master. Increments by one for each replica
+#4. Optional argument. This seq number will be skipped in execution
+
 numShards="$1"
 m="$2"
-port="$3"
+startport="$3"
+rm -f *config*
 
 if [ -z "$4" ]; then skip=-1; else skip="$4"; fi
 launchScript="maclaunchShard.sh"
 
-mydir="$PWD"
-#Start Master
-master="shardMaster.py"
-masterConfig="master_config.txt"
-rm -f $masterConfig
-echo $HOSTNAME $port >> $masterConfig
-#gnome-terminal -e "./$master $port $numShards"
-# ./$master $port $numShards &
-osascript -e 'tell application "Terminal" to do script "cd '$mydir';./'$master' '$port' '$numShards'"'
-let port=port+1
-
+let port=$startport+1
 # Bring up the shards
 shard=0
 while [ $shard -lt $numShards ]; 
 do
-  #open -a Terminal ./$launchScript --agrs $m $port $shard $skip
-  # ./$launchScript $m $port $shard $skip &
-  osascript -e 'tell application "Terminal" to do script "cd '$mydir';./'$launchScript' '$m' '$port' '$shard' '$skip'"'
+  ./$launchScript $m $port $shard $skip
   let port=port+$m
   let shard=shard+1
 
 done
 
-
+#Start Master
+master="shardMaster.py"
+masterConfig="master_config.txt"
+mydir="$PWD"
+#Start Master
+rm -f $masterConfig
+echo $HOSTNAME $startport >> $masterConfig
+# gnome-terminal -e "./$master $startport $numShards"
+osascript -e 'tell application "Terminal" to do script "cd '$mydir';./'$master' '$startport' '$numShards'"'
